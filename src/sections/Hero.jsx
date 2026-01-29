8c2d693b931c2a56624bf2431c7dc84a20d49935
Mayur <120499580+Mayur0018@users.noreply.github.com>
2026-01-29 13:27:32 +0530

    ui-changes

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,5 @@
 import { Canvas, useFrame } from "@react-three/fiber";
-import HeroText from "../components/HeroText";
+import HeroText from "../components/HeroText.jsx"
 import ParallaxBackground from "../components/ParallaxBackground";
 import { Astronaut } from "../components/Astronaut";
 import { Float } from "@react-three/drei";
